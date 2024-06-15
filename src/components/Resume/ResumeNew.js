2e2a239be323c2dc62b8e1a5902a15a1bb9de9dd
vivekkr277 <vkr70611@gmail.com>
2024-06-15 20:59:16 +0530

    updated project components

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/Vivek_Resume.pdf";
+import pdf from "../../Assets/vivek resume 2.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
